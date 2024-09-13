f826a62d8c1ffce105ed5214c6e1fb82d5bb6ceb
Ayoub Achak <ayoub.achak01@gmail.com>
2024-09-13 17:21:08 +0200

    removed some extra comments

--- models.py
+++ models.py
@@ -13,11 +13,10 @@ from primitives import disk,cylinder,cone, torus, cube_colored, stick, rectangle
 
 # TODO : create 3D axe with disk,cylinder,cone
 def axe(base, height, slices=10, stacks=5):
-   # Adjust these values based on how you want the axe to look
    base_radius = base
    handle_height = height * 0.8
    head_height = height * 0.2
-   head_base = base * 1.01  # Wider than the handle
+   head_base = base * 1.01  
 
    # Handle
    glPushMatrix()
@@ -57,21 +56,17 @@ def wcs_zxy(size):
 
 # TODO : create bolts 
 def bolt(radius, height):
-    # Set a unique color for the bolt, e.g., silver
     stick(radius, radius, height, 7)
     glPushMatrix()
     glTranslatef(0, 0, height)
-    # No need for a separate disk on top as stick includes both ends
     glPopMatrix()
 
 # TODO : create wheel with bolts 
 def wheel(size, n_bolts=5):
-   # Set a unique color for the wheel, e.g., black
    glColor3f(0.0, 0.0, 0.0)  # Black color
    radius = size / 2
    height = size / 4
    
-   # Use cylinder for the wheel body
    cylinder(radius, radius, height, 20, 5)
    
    # Add a disk on the top of the cylinder
@@ -80,10 +75,9 @@ def wheel(size, n_bolts=5):
    disk(0, radius, 20, 5)
    glPopMatrix()
 
-   # Add a disk on the bottom of the cylinder (to show from the opposite side)
+   # Add a disk on the bottom of the cylinder (
    glPushMatrix()
-   glTranslatef(0, 0, 0)  # No need to translate since it's the base
-   glRotatef(180, 1, 0, 0)  # Rotate to make it face downwards
+   glRotatef(180, 1, 0, 0) 
    disk(0, radius, 20, 5)
    glPopMatrix()
 
@@ -92,22 +86,18 @@ def wheel(size, n_bolts=5):
       angle = 360 / n_bolts * i
       glPushMatrix()
       glRotatef(angle, 0, 0, 1)
-      # Adjust the translation to place bolts on the surface
-      glTranslatef(radius - (height * 0.5), 0, height / 2)  # Adjusted translation
-      bolt(radius / 10, height)  # Adjust bolt height if necessary
+      glTranslatef(radius - (height * 0.5), 0, height / 2)  
+      bolt(radius / 10, height)  
       glPopMatrix()
 
 # TODO : create car : body (axe) + 4 wheels
 def car(size):
 
-   # Add wheels to the car
    glPushMatrix()
  
-   # Créer le corps de la voiture (ici un cube coloré, mais tu peux utiliser une autre primitive)
    glTranslatef(0, size, 0)
    glScalef(2, 1, 1)
-   glColor3f(0.8, 0, 0) # Couleur rouge pour le corps
-   cube_colored(size) # Utiliser un cube pour représenter le corps de la voiture
+   cube_colored(size)
 
    # Placer les roues
    wheel_size = size * 0.5
